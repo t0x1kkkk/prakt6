8694668243fb1d9c274710b6f1ffd2924a0c5e5a
t0x1kkkk <daniil.fridrik@gmail.com>
2023-07-07 16:43:28 +0300

    prakt6

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int x1, y1, x2, y2;
+    printf("Введіть координати початку вектора (x1 y1): ");
+    scanf("%d %d", &x1, &y1);
+    printf("Введіть координати кінця вектора (x2 y2): ");
+    scanf("%d %d", &x2, &y2);
+    double length = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+    printf("Довжина вектора: %.6lf\n", length);
+    return 0;
+}
